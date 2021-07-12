3d430fd35304d0105f7b2e2c25edec6b54d83478
daria888-hub <daria.lee365@gmail.com>
2021-07-12 15:34:19 +1000

    add forecast API

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ function city(event) {
   searchCity(city);
 }
 
-function displayForecast (){
+function displayForecast (response){
+  console.log( response.data.daily)
   let forecastElement=document.querySelector ( "#forecast");
 
 let forecastHTML =`<div class="row">`;
@@ -75,9 +76,17 @@ forecastHTML+ `
   forecastElement.innerHTML=forecastHTML
 };
 
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metrics`;
+  axios.get(apiUrl).then(displayForecast);
 
+
+};
 
 function showTempreture(response) {
+  
   document.querySelector("h1").innerHTML = response.data.name;
 
   document.querySelector("#currentTemp").innerHTML = Math.round(
@@ -98,7 +107,10 @@ function showTempreture(response) {
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
-   celsiusTemp =  Math.round(response.data.main.temp);  }
+   celsiusTemp =  Math.round(response.data.main.temp);  
+  
+   getForecast(response.data.coord)
+  }
 
    
 
@@ -142,7 +154,7 @@ let search = document.querySelector("#search-city-form");
 search.addEventListener("submit", city);
 
 searchCity("Sydney");
-displayForecast();
+
 
 let currentLocationButton = document.querySelector("button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
